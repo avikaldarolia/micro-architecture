b4d46a372d15b2bf09ee961458c66796eee74713
Avikal Darolia <avikaldarolia7@gmail.com>
2024-10-10 19:32:46 -0400

    Renaming

--- log-server/index.js
+++ log-server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectRabbitMQ } from "./rabbitmq.js";
-import { query } from "../Base server/db.js";
+import { query } from "../base-server/db.js";
 
 dotenv.config();
 
